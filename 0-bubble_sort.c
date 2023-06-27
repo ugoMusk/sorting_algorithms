5a0a3fa0b5f45270366244838d59fb022e24d991
ugoMusk <ugobothankgod@gmail.com>
2023-06-27 19:14:01 +0100

    update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,11 @@
  */
 void swap_items(int *array, size_t x, size_t y)
 {
-        int temp;
+  int temp;
 
-        temp = array[x];
-        array[x] = array[y];
-        array[y] = temp;
+  temp = array[x];
+  array[x] = array[y];
+  array[y] = temp;
 }
 
 /**
@@ -25,23 +25,23 @@ void swap_items(int *array, size_t x, size_t y)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int swapped = 0;
+  size_t i, j;
+  int swapped = 0;
 
-	if (array == NULL || size < 2)
-		return;
-	for (i = 1; i < size; i++)
+  if (array == NULL || size < 2)
+    return;
+  for (i = 1; i < size; i++)
+    {
+      for (j = 0; j < size - i; j++)
 	{
-		for (j = 0; j < size - i; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap_items(array, j, j + 1);
-				print_array(array, size);
-				swapped++;
-			}
-		}
-		if (i == 1 && swapped == 0)
-			return;
+	  if (array[j] > array[j + 1])
+	    {
+	      swap_items(array, j, j + 1);
+	      print_array(array, size);
+	      swapped++;
+	    }
 	}
+      if (i == 1 && swapped == 0)
+	return;
+    }
 }
