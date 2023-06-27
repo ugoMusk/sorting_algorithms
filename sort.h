6f55637552725a7fe3413b7a0b9d345d807ee9fb
ugoMusk <ugobothankgod@gmail.com>
2023-06-27 19:00:01 +0100

    rewrite header file

--- sort.h
+++ sort.h
@@ -1,9 +1,12 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
 #include <stdio.h>
-#include "print_array.c"
+#include <stdlib.h>
+
+/* macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,18 +22,24 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
 void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void insert_behind(listint_t *node, listint_t *key);
-listint_t *pop_node(listint_t *key);
-void swap(int *array, size_t x, size_t y);
-void select_swap(int *array, size_t x, size_t y);
-void quick_swap(int *array, size_t x, size_t y);
-size_t get_partition(int *array, size_t size, int start, int end);
-void do_quick_sort(int *array, size_t size, size_t start, size_t end);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);
+
+/* advanced tasks */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
